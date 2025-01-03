d964a9c300410effb755d99725bcda6ae05b02de
Daniel Cunha <danielsoro@gmail.com>
2025-01-02 22:33:36 -0300

    feat(log): supporte to enable or disable debug logs

--- main.go
+++ main.go
@@ -8,7 +8,20 @@ import (
 )
 
 func main() {
+	logLevel := os.Getenv("WCLI_DEBUG")
+
+	if len(logLevel) > 0 {
+		logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+
+		slog.SetDefault(logger)
+		cmd.Execute()
+		return
+	}
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
 	cmd.Execute()
 }
