7706b63208559f91fc297b99f77313e992076b4f
Daniel Cunha <danielsoro@gmail.com>
2025-01-04 17:46:57 -0300

    refactor(root): Adding new help functions to root command

--- cmd/root.go
+++ cmd/root.go
@@ -15,31 +15,46 @@ var rootCommand = &cobra.Command{
 
 func init() {
 	config.NewConfig()
+	definePersistentFlags()
+	bindValuesWithViper()
+	makeFlagsRequired()
+}
 
-	rootCommand.PersistentFlags().StringP("config", "c", "~/.config/wordpress-cli/config", "The configuration file to use")
+func definePersistentFlags() {
 	rootCommand.PersistentFlags().StringP("username", "u", "", "The wordpress username to use the Wordpress Rest API")
 	rootCommand.PersistentFlags().StringP("password", "p", "", "The wordpress password to use the Wordpress Rest API")
 	rootCommand.PersistentFlags().StringP("wordpress-url", "", "", "The wordpress url REST Api")
+}
 
-	err := viper.BindPFlag("credentials.username", rootCommand.PersistentFlags().Lookup("username"))
-	if err != nil {
-		slog.Debug("Not possible to bind username flag with config file: ", slog.String("error", err.Error()))
+func bindValuesWithViper() {
+	viper.BindPFlag("credentials.username", rootCommand.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("credentials.password", rootCommand.PersistentFlags().Lookup("password"))
+	viper.BindPFlag("url", rootCommand.PersistentFlags().Lookup("wordpress-url"))
+}
+
+func makeFlagsRequired() {
+	if len(viper.GetString("url")) == 0 {
+		rootCommand.MarkPersistentFlagRequired("wordpress-url")
 	}
 
-	err = viper.BindPFlag("credentials.password", rootCommand.PersistentFlags().Lookup("password"))
-	if err != nil {
-		slog.Debug("Not possible to bind password flag with config file: ", slog.String("error", err.Error()))
+	if len(viper.GetString("credentials.username")) == 0 {
+		rootCommand.MarkPersistentFlagRequired("username")
 	}
 
-	err = viper.BindPFlag("url", rootCommand.PersistentFlags().Lookup("wordpress-url"))
-	if err != nil {
-		slog.Debug("Not possible to bind wordpress-url flag with config file: ", slog.String("error", err.Error()))
+	if len(viper.GetString("credentials.password")) == 0 {
+		rootCommand.MarkPersistentFlagRequired("password")
 	}
+}
+
+func RemoveRootRequiredFlags(c *cobra.Command) {
+	c.InheritedFlags().SetAnnotation("username", cobra.BashCompOneRequiredFlag, []string{"false"})
+	c.InheritedFlags().SetAnnotation("password", cobra.BashCompOneRequiredFlag, []string{"false"})
+	c.InheritedFlags().SetAnnotation("wordpress-url", cobra.BashCompOneRequiredFlag, []string{"false"})
 }
 
 func Execute() {
 	err := rootCommand.Execute()
 	if err != nil {
-		slog.Debug("Not possible to start CLI", slog.String("error", err.Error()))
+		slog.Error("Not possible to start CLI", slog.String("error", err.Error()))
 	}
 }
