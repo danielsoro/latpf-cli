e042b470d36b25e59f3946834a88cacf96c83f15
Daniel Cunha <danielsoro@gmail.com>
2025-02-21 18:30:20 -0300

    Initial test with options and fix to the bin generation

--- src/posts/import.ts
+++ src/posts/import.ts
@@ -3,6 +3,15 @@ import { Command } from "commander";
 export class ImportCommandFactory {
   public static create(postCommand: Command) {
     postCommand.command('import')
+        .option('-f, --file-path <filepath>', 'Path of the file to be imported')
+        .action(importPost)
   }
 }
 
+interface ImportArguments {
+  filePath: string
+}
+
+function importPost(args: ImportArguments) {
+  console.log(args.filePath)
+}
